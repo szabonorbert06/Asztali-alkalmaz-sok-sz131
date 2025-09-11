023a23f64bbbde1dc1eb89bb248199b8b37a8fb4
szabonorbert06 <106810720+szabonorbert06@users.noreply.github.com>
2025-09-11 11:00:23 +0200

    Add files via upload

---
+++ orai1/orai1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace orai1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            do
+            {
+                Console.Write("1.");
+                int m = 0;
+                while(int.TryParse(Console.ReadLine(), out m));
+
+                Console.Write("2.");
+                int n = 0;
+                while(int.TryParse(Console.ReadLine(), out n)); 
+
+                int r = m % n; 
+                int.TryParse(Console.ReadLine(), out r);
+ 
+                while (r != 0)
+                {
+                    {
+                        m = n;
+                        n = r;
+                        r = m % n;
+                    }
+                    Console.WriteLine($"lnko: {n}");
+
+                }
+
+            } while (true);
+        }
+    }
+}
+
+        
---
+++ orai1/orai1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("orai1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("orai1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f92bf33a-88b7-400f-9dc2-5c7579732c4b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
